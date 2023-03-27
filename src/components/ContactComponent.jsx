0b8b8e72914f9ad23b82df724c3e210062d7eed2
Erik <erik.ondra@yahoo.com>
2023-03-27 19:28:30 +0200

    safari adjustemnts

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -21,60 +21,64 @@ const ContactComponent = () => {
             <h2 className="mt-3 mb-5">Get in touch</h2>
           </Col>
         </Row>
-        <Row className="justify-content-around align-items-center">
-          <Image
-            src="https://res.cloudinary.com/dyy38u8x7/image/upload/v1679845249/portfolio/kindpng_3938892_yfmmkr.png"
-            alt="get in touch logo"
-            className="mail-logo mb-5"
-            fluid
-          ></Image>
+        <Row className="align-items-center">
+          <Col className="d-flex justify-content-center">
+            <Image
+              src="https://res.cloudinary.com/dyy38u8x7/image/upload/v1679845249/portfolio/kindpng_3938892_yfmmkr.png"
+              alt="get in touch logo"
+              className="mail-logo mb-5"
+              fluid
+            ></Image>
+          </Col>
 
-          <Form onSubmit={handleSubmit} className="text-left contact-form">
-            <Form.Group controlId="formName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
+          <Col className="d-flex justify-content-center contact-form-col">
+            <Form onSubmit={handleSubmit} className="text-left contact-form">
+              <Form.Group controlId="formName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="formEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="formEmail">
-              <Form.Label>Subject</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter your email"
-                value={subject}
-                onChange={(e) => setSubject(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Subject</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="formMessage">
-              <Form.Label>Message</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                placeholder="Enter your message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-            </Form.Group>
+              <Form.Group controlId="formMessage">
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Enter your message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+              </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Send
-            </Button>
-          </Form>
+              <Button variant="primary" type="submit">
+                Send
+              </Button>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </div>
